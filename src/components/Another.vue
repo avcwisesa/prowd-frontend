6d0ea576a58711ff0dc50eb12bdf2522f9916e7c
Avicenna Wisesa <avcwisesa@gmail.com>
2019-01-27 11:39:32 +0700

    finished multidimensional view prototype

--- src/components/Another.vue
+++ src/components/Another.vue
@@ -1,39 +1,41 @@
 <template>
-  <v-layout column justify-center align-center>
+  <v-layout column align-center>
     <h1 class="ml-3 mb-3 pt-4"> {{profileName}} </h1>
-    <!-- <div  v-for="f1value in f1values" v-bind:key=f1value>
+    <div  v-for="f1value in f1values" v-bind:key=f1value>
       <v-flex xs12>
         <v-card>
           <v-card-title class="headline">{{f1value}}</v-card-title>
           <v-layout row>
             <v-flex xs4 v-for="f2value in f2values" v-bind:key=f2value>
               <v-card-title class="headline">{{f2value}} </v-card-title>
-              <v-card-text>Amount of entities according to completeness score</v-card-text>
-              <BarChart :chart-data="datacollection" :options="chartOptions"/>
+              <v-card-text :id="f1value + f2value + 'amount'">Amount of entities in this facet: </v-card-text>
+              <canvas :id="f1value + f2value"></canvas>
             </v-flex>
-            <canvas id="chart1"></canvas>
-            <canvas id="chart2"></canvas>
           </v-layout>
         </v-card>
       </v-flex>
-    </div> -->
-    <canvas id="chart1"></canvas>
-    <canvas id="chart2"></canvas>
-    <!-- {{ datacollection }} -->
-    <v-btn @click="createChart('chart1')" color="success"> 1 </v-btn>
-    <v-btn @click="createChart('chart2')" color="info"> 2 </v-btn>
+    </div>
 
   </v-layout>
 </template>
 
 <script>
 import Chart from 'chart.js'
+/* eslint-disable */
 
 export default {
   name: 'App',
+  async fetch ({ store }) {
+    await Promise.all([
+      store.dispatch('LANGUAGES'),
+      store.dispatch('FETCH_PROFILE_BY_ID', 106)
+    ])
+  },
   data () {
       return {
-        profileName: 'Politician - ID',
+        f1v: [],
+        f2v: [],
+        f3v: [],
         options: {
           responsive: true,
           lineTension: 1,
@@ -42,62 +44,233 @@ export default {
               ticks: {
                 beginAtZero: true,
                 padding: 25,
+                suggestedMax: 100,
               }
             }]
           }
         },
-        chartData: {
-          labels: ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
-          datasets: [
-            { // one line graph
-              label: 'Number of Moons',
-              data: [0, 0, 1, 2, 67, 62, 27, 14],
-              backgroundColor: [
-                'rgba(54,73,93,.5)', // Blue
-                'rgba(54,73,93,.5)',
-                'rgba(54,73,93,.5)',
-                'rgba(54,73,93,.5)',
-                'rgba(54,73,93,.5)',
-                'rgba(54,73,93,.5)',
-                'rgba(54,73,93,.5)',
-                'rgba(54,73,93,.5)'
-              ],
-              borderColor: [
-                '#36495d',
-                '#36495d',
-                '#36495d',
-                '#36495d',
-                '#36495d',
-                '#36495d',
-                '#36495d',
-                '#36495d',
-              ],
-              borderWidth: 3
-            },
-            { // another line graph
-              label: 'Planet Mass (x1,000 km)',
-              data: [4.8, 12.1, 12.7, 6.7, 139.8, 116.4, 50.7, 49.2],
-              backgroundColor: [
-                'rgba(71, 183,132,.5)', // Green
-              ],
-              borderColor: [
-                '#47b784',
-              ],
-              borderWidth: 3
-            }
-          ]
-        }
+        datacollection: {}
       }
   },
+  computed: {
+    entities () {
+      var entities = JSON.parse(JSON.stringify(this.$store.state.entities1))
+      // var attributes = JSON.parse(JSON.stringify(this.$store.state.attributes))
+
+      // entities.forEach(function (entity) {
+      //   entity.classLabel = entity.classLabel.value
+      //   entity.score = (100 * (Object.keys(entity).length - 2) / attributes.length)
+      // })
+
+      return entities
+    },
+    headers () {
+      var entityClass = this.$store.state.class
+      var ret = [].concat(this.$store.state.attributes)
+      ret = ret.map(obj => {
+        return { text: obj.name + ' (' + obj.code + ')', value: obj.code + 'Exist' }
+      })
+      ret = [{ text: entityClass.name + ' (' + entityClass.code + ')', value: 'classLabel' }].concat(ret)
+      var headers = ret.concat({ text: 'completeness score', value: 'score' })
+      return headers
+    },
+    languages () {
+      return this.$store.state.languages
+    },
+    attributeVariables () {
+      var attrs = this.$store.state.attributes
+      attrs = attrs.map(obj => obj.code + 'Exist')
+      return ['classLabel'].concat(attrs)
+    },
+    barChartData () {
+      return this.$store.state.barChartData
+    },
+    class () {
+      return this.$store.state.class
+    },
+    facets () {
+      return this.$store.state.facets
+    },
+    attributes () {
+      var entities = this.$store.state.entities1
+      var attributes = this.$store.state.attributes
+      var amount = {}
+
+      attributes.forEach(function (attr) {
+        amount[attr.code] = 0
+      })
+
+      entities.forEach(function (entity) {
+        attributes.forEach(function (attr) {
+          if (entity[attr.code + 'Exist']) {
+            amount[attr.code] += 1
+          }
+        })
+      })
+
+      attributes.forEach(function (attr) {
+        attr.count = amount[attr.code]
+        attr.score = (100 * attr.count / entities.length)
+      })
+
+      return attributes.sort(function (a, b) {
+        return a.count - b.count
+      })
+    },
+    profileName () {
+      return this.$store.state.profileName
+    },
+    facetOptions () {
+      return this.facetOptionsData
+    },
+    subclass () {
+      return this.$store.state.subclass
+    },
+    f1values () {
+      return Array.from(this.$data.f1v)
+    },
+    f2values () {
+      return Array.from(this.$data.f2v)
+    }
+  },
   methods: {
-    createChart (chartId) {
-      const ctx = document.getElementById(chartId);
+    f (f1, f2, entities) {
+      var f1s = []
+      var f2s = []
+      var result = {}
+
+      entities.forEach((entity) => {
+        if (entity[f1 + 'Label']) {
+          f1s.push(entity[f1 + 'Label'].value)
+        }
+        if (entity[f2 + 'Label']) {
+          f2s.push(entity[f2 + 'Label'].value)
+        }
+      })
+      f1s.push('none')
+      f2s.push('none')
+
+      f1s = new Set(f1s)
+      f2s = new Set(f2s)
+
+      this.$data.f1v = Array.from(f1s)
+      this.$data.f2v = Array.from(f2s)
+
+      f1s.forEach((elem1) => {
+        result[elem1] = {}
+        f2s.forEach((elem2) => {
+          result[elem1][elem2] = []
+        })
+      })
+
+      entities.forEach((entity) => {
+        var label1 = entity[f1 + 'Label'] || { value: 'none' }
+        var label2 = entity[f2 + 'Label'] || { value: 'none' }
+
+        result[label1.value][label2.value].push(entity)
+      })
+
+      return result
+    },
+    createChart (chartId, chartData) {
+      const ctx = document.getElementById(chartId)
+      // console.log(ctx)
       new Chart(ctx, {
-        type: 'line',
-        data: this.$data.chartData,
+        type: 'bar',
+        data: chartData,
         options: this.$data.options,
       })
+    },
+    postQuery () {
+      this.loading = true
+
+      var query = `
+        SELECT ?entity ?entityLabel ?p69Label ?p102Label ?p21Label ?p19Label ?p1412Label
+        WHERE {
+          ?entity wdt:P31 wd:Q5.
+          ?entity wdt:P106 wd:Q82955.
+          ?entity wdt:P27 wd:Q252.
+          OPTIONAL {?entity wdt:P69 ?p69}
+          OPTIONAL {?entity wdt:P102 ?p102}
+          OPTIONAL {?entity wdt:P21 ?p21}
+          OPTIONAL {?entity wdt:P19 ?p19}
+          OPTIONAL {?entity wdt:P1412 ?p1412}
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+        }
+      `
+
+      this.$axios.post('https://query.wikidata.org/sparql?query=' + encodeURIComponent(query))
+        .then((response) => {
+          var entities = response.data.results.bindings
+          console.log('================================LOLOLOLOLOL')
+          this.$store.commit('SET_ENTITIES1', entities)
+
+          entities.forEach((entity) => {
+            var exist = Object.keys(entity).length - 2
+            entity.score = (exist / 5) * 100
+          })
+
+          var dataset = this.f('p102', 'p21', entities)
+          console.log(dataset)
+          var attributes = ['p1412', 'p69', 'p19']
+
+          this.$data.f1v.forEach((value1) => {
+            this.$data.datacollection[value1] = {}
+            this.$data.f2v.forEach((value2) => {
+              if (typeof dataset[value1][value2] == 'undefined') {
+                return
+              }
+              var chartData = {
+                labels: attributes,
+                datasets: []
+              }
+              var data = []
+              var subset = dataset[value1][value2]
+              var size = subset.length
+
+              if (size == 0) {
+                return
+              }
+
+              attributes.forEach((attribute) => {
+                var sum = subset.reduce((acc, entity) => {
+                  var attributeExists = entity[attribute + 'Label'] || 'none'
+                  if (attributeExists == 'none') {
+                    return acc
+                  } else {
+                    return acc + 1
+                  }
+                }, 0)
+                data.push(sum / size * 100)
+              })
+
+              // console.log(data)
+              chartData.datasets.push({
+                label: 'Average Completeness',
+                backgroundColor: '#41b883',
+                data: data
+              })
+
+              // console.log(JSON.stringify(chartData))
+              this.$nextTick(() => {
+                const ctx = document.getElementById(value1 + value2 + 'amount')
+                ctx.innerText = ctx.innerText + size
+                this.createChart(value1 + value2, chartData)
+              })
+            })
+          })
+
+          this.loading = false
+          // this.warning = (this.entities.length === 20000)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     }
+  },
+  mounted: function () {
+    this.postQuery()
+    // this.$forceUpdate()
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,208 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+import axios from 'axios'
 
+Vue.use(Vuex)
+/* eslint-disable */
 export default new Vuex.Store({
   state: {
-
+    alertValue: false,
+    alertMessage: 'alert',
+    class: {},
+    classElements: [],
+    facets: [],
+    facetOptions: {},
+    attributes: [],
+    filters: [],
+    score1: 0,
+    score2: 0,
+    subclass: false,
+    entities1: [],
+    entities2: [],
+    properties: [1, 2, 3, 4, 5, 6],
+    error: null,
+    profileName: '',
+    profileClass: '',
+    profiles: [],
+    suggestedEntity: [],
+    suggestedProperty: [],
+    suggestedPropertyEdit: [],
+    jumbotron: false,
+    languages: []
   },
   mutations: {
-
+    toggleSidebar (state) {
+      state.sidebar = !state.sidebar
+    },
+    SET_FAILURE (state, error) {
+      state.error = error
+    },
+    SET_ENTITIES1 (state, entities) {
+      state.entities1 = entities
+    },
+    SET_ENTITIES2 (state, entities) {
+      state.entities2 = entities
+    },
+    SET_SCORE1 (state, score) {
+      state.score1 = score
+    },
+    SET_SCORE2 (state, score) {
+      state.score2 = score
+    },
+    SET_PROFILENAME (state, name) {
+      state.profileName = name
+    },
+    SET_CLASSNAME (state, name) {
+      state.className = name
+    },
+    SET_CLASS (state, classString) {
+      state.class = JSON.parse(classString)
+    },
+    SET_ATTRIBUTES (state, attrString) {
+      state.attributes = JSON.parse(attrString)
+    },
+    SET_FACETS (state, facetString) {
+      state.facets = JSON.parse(facetString)
+    },
+    SET_PROFILES (state, profiles) {
+      state.profiles = profiles
+    },
+    SET_SUGGESTED_ENTITY (state, suggestion) {
+      state.suggestedEntity = suggestion
+    },
+    SET_SUGGESTED_PROPERTY (state, suggestion) {
+      suggestion.forEach(element => {
+        element.name = element.label
+        element.code = element.id
+      })
+      state.suggestedProperty = suggestion
+    },
+    SET_FACET_OPTIONS (state, {facet, options}) {
+      state.facetOptions[facet] = options
+    },
+    SET_SUBCLASS (state, subclass) {
+      state.subclass = subclass
+    },
+    SET_FILTERS (state, filters) {
+      state.filters = JSON.parse(filters)
+    },
+    SET_ALERT_VALUE (state, value) {
+      state.alertValue = value
+    },
+    SET_ALERT_MESSAGE (state, message) {
+      state.alertMessage = message
+    },
+    SET_JUMBOTRON_VALUE (state, value) {
+      state.jumbotron = value
+    },
+    SET_LANGUAGES (state, languages) {
+      state.languages = languages
+    }
   },
   actions: {
+    FETCH_PROFILE_BY_ID ({commit}, id) {
+      console.log('fetching2 ID: ' + id)
+      return axios.get(process.env.API_ENDPOINT + '/profile/' + id)
+        .then((response) => {
+          commit('SET_CLASS', response.data.class)
+          commit('SET_ATTRIBUTES', response.data.attributes)
+          commit('SET_FACETS', response.data.facets)
+          commit('SET_PROFILENAME', response.data.name)
+          commit('SET_SUBCLASS', response.data.subclass)
+          commit('SET_FILTERS', response.data.filters)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    // FETCH_PROFILE ({commit}) {
+    //   console.log('fetching all profiles')
+    //   return axios.get(process.env.API_ENDPOINT + '/profile')
+    //     .then((response) => {
+    //       commit('SET_PROFILES', response.data)
+    //     })
+    //     .catch((error) => {
+    //       console.log(error)
+    //     })
+    // },
+    // CREATE_PROFILE ({commit}, newProfile) {
+    //   return axios.post(process.env.API_ENDPOINT + '/profile/new', newProfile)
+    //     .then((response) => {
+    //       console.log(newProfile)
+    //       commit('SET_ALERT_VALUE', true)
+    //       commit('SET_ALERT_MESSAGE', `Profile successfully created`)
+    //       console.log(response)
+    //     }).catch((error) => {
+    //       console.log(error)
+    //     })
+    // },
+    // UPDATE_PROFILE ({commit}, { id, profile }) {
+    //   return axios.put(process.env.API_ENDPOINT + '/profile/' + id, profile)
+    //     .then((response) => {
+    //       console.log(profile)
+    //       commit('SET_ALERT_VALUE', true)
+    //       commit('SET_ALERT_MESSAGE', `Profile successfully updated`)
+    //       console.log(response)
+    //     }).catch((error) => {
+    //       console.log(error)
+    //     })
+    // },
+    // FETCH_FACET_OPTIONS ({commit, state}) {
+    //   var responsePromise = state.facets.map(async (facet) => {
+    //     var query = `
+    //     SELECT DISTINCT ?facet ?facetLabel ?${facet.code}
+    //         WHERE {
+    //         ?entity wdt:P31 wd:${state.class.code}.
+    //         ?entity wdt:${facet.code} ?facet.
+    //         SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    //         }
+    //     `
+    //     return axios.post(process.env.WIKIDATA_SPARQL_ENDPOINT + 'sparql?query=' + encodeURIComponent(query))
+    //   })
+    //   console.log(responsePromise)
 
+    //   Promise.all(responsePromise).then((completed) => {
+    //     completed.map((response) => {
+    //       commit('SET_FACET_OPTIONS', { facet: response.data.head.vars[2], options: response.data.results.bindings })
+    //     })
+    //   })
+    // },
+    // SUGGESTER ({commit}, { type, query }) {
+    //   return axios.post(process.env.WIKIDATA_API_ENDPOINT + `?action=wbsearchentities&format=json&origin=*&type=${type}&search=${query}&language=en`)
+    //     .then((response) => {
+    //       var commitMethod = ''
+    //       if (type === 'item') {
+    //         commitMethod = 'SET_SUGGESTED_ENTITY'
+    //       } else {
+    //         commitMethod = 'SET_SUGGESTED_PROPERTY'
+    //       }
+    //       commit(commitMethod, response.data.search)
+    //     }).catch((error) => {
+    //       console.log(error)
+    //     })
+    // },
+    LANGUAGES ({commit}) {
+      return axios.get(process.env.WIKIDATA_API_ENDPOINT + `?action=query&format=json&origin=*&meta=siteinfo&siprop=languages`)
+        .then((response) => {
+          console.log(response.data.query.languages)
+          commit('SET_LANGUAGES', response.data.query.languages)
+        }).catch((error) => {
+          console.log(error)
+        })
+    },
+    // DELETE_PROFILE ({commit}, { name, id }) {
+    //   console.log('deleting: ' + id)
+    //   return axios.delete(process.env.API_ENDPOINT + '/profile/' + id)
+    //     .then((response) => {
+    //       commit('SET_ALERT_VALUE', true)
+    //       commit('SET_ALERT_MESSAGE', `Profile '${name}' successfully deleted`)
+    //     })
+    //     .catch((error) => {
+    //       commit('SET_ALERT_VALUE', true)
+    //       commit('SET_ALERT_MESSAGE', `Failure in deleting profile '${name}'`)
+    //       console.log(error)
+    //     })
+    // }
   }
 })
